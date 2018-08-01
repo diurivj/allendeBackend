773a54b9787595c3210b8d61621f1159e5a43bd0
diurivj <diurivj@icloud.com>
2018-07-31 20:52:25 -0500

    polishing app.js

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const passport     = require('./helpers/passport');
 require('dotenv').config();
-
 
 mongoose.Promise = Promise;
 mongoose
-  .connect(process.env.DB, {useMongoClient: true})
+  .connect('mongodb://diuri:diuri@allendeback-shard-00-00-od8n8.mongodb.net:27017,allendeback-shard-00-01-od8n8.mongodb.net:27017,allendeback-shard-00-02-od8n8.mongodb.net:27017/test?ssl=true&replicaSet=allendeBack-shard-0&authSource=admin&retryWrites=true', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -25,15 +25,10 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-//env
-
-// Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Express View engine setup
 
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
@@ -41,28 +36,19 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 
-//passport
-const passport = require('./helpers/passport');
 app.use(passport.initialize());
 app.use(passport.session());
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
-// default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
-app.use('/', index);
 const auth = require('./routes/auth');
+app.use('/', index);
 app.use('/', auth);
-
 
 module.exports = app;
