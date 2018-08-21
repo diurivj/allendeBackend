6a4faa5e94c4366a5653d2c3f6925fd382ecd848
Alberto <zonanabeto@gmail.com>
2018-08-21 17:27:37 -0500

    jalale 1

--- routes/dist.js
+++ routes/dist.js
@@ -2,16 +2,12 @@ const router  = require('express').Router();
 const User = require('../models/User');
 
 router.get('/distribuidores', (req, res) => {
+  console.log("perro");
   User.find()
   .then(dist => res.json(dist))
   .catch(e => console.log(e))
 });
 
-router.post('/new-dist', (req, res, next) => {
-  User.create(req.body)
-  .then(dist => res.json(dist))
-  .catch(e => next(e))
-});
 
 
 
